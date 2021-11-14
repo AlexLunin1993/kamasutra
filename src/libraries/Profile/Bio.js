5bcb77379c243f891a76446d9be21b1bb3392bb5
AlexLunin1993 <alex.lunin.1993@mail.ru>
2021-11-14 08:20:47 +0300

    'setState'

--- src/libraries/Profile/Bio.js
+++ src/libraries/Profile/Bio.js
@@ -1,10 +1,11 @@
 import './Bio.css';
 
 const Bio = (props) => {
+    debugger
     return (
         <div className='Bio'>
-            <p><b>Имя:</b> </p>
-            <p><b>Возраст:</b> </p>
+            <p><b>Имя:</b> {props.name} </p> 
+            <p><b>Статус:</b> {props.status} </p> 
         </div>);
 }
 
--- src/libraries/Profile/Profile.js
+++ src/libraries/Profile/Profile.js
@@ -1,7 +1,7 @@
 
 import Preloader from '../common/preloader/preloader.js';
 import Avatar from './Avatar.js';
-import Bio from './Bio.js';
+import ProfileStatus from './ProfileStatus.js';
 import MyPostsContainer from './MyPostsContainer';
 
 const Profile = (props) => {
@@ -9,11 +9,16 @@ const Profile = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
+    debugger
     return (
 
         <div>
             <Avatar photo={props.profile.photos.large} />
-            <Bio name='Барсик' age='вечно молодой' race='бродяга'/>
+            {/* <Bio name={props.profile.fullName} status={props.profile.aboutMe}/> */}
+            <p><b>Имя:</b> {props.profile.fullName} </p> 
+            <p><b>Статус:</b> {props.profile.aboutMe} </p> 
+            <ProfileStatus status = {props.profile.aboutMe} />
+            <p />
             <MyPostsContainer/>
         </div>);
 
